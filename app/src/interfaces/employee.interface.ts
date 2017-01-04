74b3b3002c0b5b4c7fad4b1b2e00af72d80f23f6
Raju-N <rajuece25@gmail.com>
2017-01-04 15:02:57 +0530

    Add files via upload

---
+++ app/src/interfaces/employee.interface.ts
@@ -0,0 +1,11 @@
+export interface employee {
+    empid: number;
+    empname: string;
+    phone: number;
+    email : string;
+    designation: string;
+    location: string;
+    address: string;
+    experience: number;
+    skill: any;
+}
